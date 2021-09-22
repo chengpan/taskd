658b0c5b200aa2014649917f07a11dda9ed9f5f7
parker cheng <pcheng@flexport.com>
2021-09-22 12:45:48 +0800

    scaffold

---
+++ src/main/java/com/github/taskd/TaskdApplication.java
@@ -0,0 +1,13 @@
+package com.github.taskd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/taskd/TaskdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.taskd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
